10e6ea7f9095ecb6b9f1d673334405d58e448d81
Fatemeh Fazli <fatemehfazli82@gmail.com>
2022-07-25 16:40:59 +0430

    downloader

---
+++ Downloader.py
@@ -0,0 +1,9 @@
+from urllib.request import urlretrieve
+import os
+def downloader(url):
+    path = os.path.basename(url)
+    urlretrieve(url, f"C:\\Users\\ASUS\Downloads\\{path}")
+    return 'The content of this link is stored in the downloads disk'
+
+url = input("Enter your url: ")
+print(downloader(url))
